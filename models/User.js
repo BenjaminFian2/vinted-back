c48b9252084eb12f2f503c04b611debb751ff1a0
benjaminfian <48064921+benjaminfian@users.noreply.github.com>
2021-07-09 17:43:14 +0200

    fix-model-User

--- models/User.js
+++ models/User.js
@@ -14,8 +14,8 @@ const User = mongoose.model("User", {
     avatar: { type: mongoose.Schema.Types.Mixed, default: {} }, //accepte n'importe quel type de variable
   },
   token: { type: String },
-  hash: { type: String, select: false },
-  salt: { type: String, select: false },
+  hash: { type: String },
+  salt: { type: String },
 });
 
 module.exports = User;
