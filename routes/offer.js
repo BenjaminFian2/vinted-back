dbb5d7be7af8ea729af380e7d101a94adbd024c9
benjaminfian <48064921+benjaminfian@users.noreply.github.com>
2021-07-11 14:27:14 +0200

    another-fix-to-test-the-existence-of-an-image

--- routes/offer.js
+++ routes/offer.js
@@ -87,7 +87,7 @@ router.put("/offer/update/:id", isAuthenticated, async (req, res) => {
       req.fields;
     const offer = await Offer.findById(req.params.id);
     if (req.files.picture) {
-      if (offer.product_image !== null) {
+      if (offer.product_image) {
         await cloudinary.uploader.destroy(offer.product_image.public_id);
       }
       offer.product_image = await cloudinary.uploader.upload(
@@ -117,7 +117,7 @@ router.put("/offer/update/:id", isAuthenticated, async (req, res) => {
 router.delete("/offer/delete/:id", isAuthenticated, async (req, res) => {
   try {
     const offer = await Offer.findByIdAndDelete(req.params.id);
-    if (offer.product_image !== null) {
+    if (offer.product_image) {
       await cloudinary.uploader.destroy(offer.product_image.public_id);
       await cloudinary.api.delete_folder(`/vinted/offers/${offer._id}`);
     }
